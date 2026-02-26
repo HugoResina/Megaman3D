a53be29032252e6a7211ae4708f7b661a9518a0f
Hugo Resina <hugo.resina.7e7@itb.cat>
2026-02-26 10:34:15 +0100

    sp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -33,8 +33,8 @@ public class PlayerMovement : MonoBehaviour
     public float jumpCutMultiplier = 0.5f;
 
 
-    bool isItem = false;
-    bool isDoor = false;
+    bool isItem = true;
+    bool isDoor = true;
     public LayerMask JumpableLayer;
     public LayerMask ItemLayer;
     public LayerMask DoorLayer;
